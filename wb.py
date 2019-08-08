649d4841224707cd8ffaf21b7338f8b1716550e6
hensonT <henson@ishippo.com>
2019-08-08 17:40:02 +0530

    .

--- wb.py
+++ wb.py
@@ -34,31 +34,31 @@ columns = ['sku', 'brand', 'cat', 'bill', 'store', 'date', 'type', 'mrp', 'price
 #df.columns = columns
 
 
-df1 = pd.read_csv("file1.csv", names = columns, header=0)
+df1 = pd.read_csv("data/file1.csv", names = columns, header=0)
 df1['source'] = 1
 
-df2 = pd.read_csv("file2.csv", names = columns, header=0)
+df2 = pd.read_csv("data/file2.csv", names = columns, header=0)
 df2['source'] = 2
 
-df3 = pd.read_csv("file3.csv", names = columns, header=0)
+df3 = pd.read_csv("data/file3.csv", names = columns, header=0)
 df3['source'] = 3
 
-df4 = pd.read_csv("file4.csv", names = columns, header=0)
+df4 = pd.read_csv("data/file4.csv", names = columns, header=0)
 df4['source'] = 4
 
-df5 = pd.read_csv("file5.csv", names = columns, header=0)
+df5 = pd.read_csv("data/file5.csv", names = columns, header=0)
 df5['source'] = 5
 
-df6 = pd.read_csv("file6.csv", names = columns, header=0)
+df6 = pd.read_csv("data/file6.csv", names = columns, header=0)
 df6['source'] = 6
 
-df7 = pd.read_csv("file7.csv", names = columns, header=0)
+df7 = pd.read_csv("data/file7.csv", names = columns, header=0)
 df7['source'] = 7
 
-df8 = pd.read_csv("file8.csv", names = columns, header=0)
+df8 = pd.read_csv("data/file8.csv", names = columns, header=0)
 df8['source'] = 8
 
-df9 = pd.read_csv("file9.csv", names = columns, header=0)
+df9 = pd.read_csv("data/file9.csv", names = columns, header=0)
 df9['source'] = 9
 
 
@@ -74,6 +74,18 @@ overlap(df1, df6, 'brand')
 #Reasonable to assume that separate df files can be merged into a bigger df
 
 df = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8, df9])
+
+
+
+
+
+
+
+
+
+
+
+#Data cleaning and formatting
 
 df['store'] = df['store'].apply(lambda x: x[-1])
 
